935505e3a4e9d81a8b18fdb48d2d2aa7a3a602c1
Vlad Romanenko <vlad@outright.digital>
2020-06-10 22:49:48 +0300

    Install TypeORM and connect to postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AuthController } from 'src/controllers';
@@ -9,13 +10,31 @@ import { Enviroment } from 'src/models';
 import { getEnv } from 'src/environment';
 import { LocalStrategy } from 'src/common';
 
-const Envitonment: Enviroment = getEnv();
+const Environment: Enviroment = getEnv();
+
+const {
+  DB_PASSWORD,
+  DB_USERNAME,
+  DB_DATABASE_NAME,
+  DB_DATABASE_HOST
+} = process.env;
+
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: DB_DATABASE_HOST,
+      port: 5432,
+      username: DB_USERNAME,
+      password: DB_PASSWORD,
+      database: DB_DATABASE_NAME,
+      entities: [],
+      synchronize: true,
+    }),
     PassportModule,
     JwtModule.register({
-      secret: Envitonment.tokenSecret,
-      signOptions: { expiresIn: Envitonment.tokenLife },
+      secret: Environment.tokenSecret,
+      signOptions: { expiresIn: Environment.tokenLife },
     }),
   ],
   controllers: [AppController, AuthController],
